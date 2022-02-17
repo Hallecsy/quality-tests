35798f46c90723227c82ede4c64f092564c896f2
Alexis <alexismuguetpro@gmail.com>
2022-02-17 09:58:33 +0100

    :rocket: Ajout des fonctions

--- src/main/kotlin/com/teaching/project/banking/domain/Account.kt
+++ src/main/kotlin/com/teaching/project/banking/domain/Account.kt
@@ -6,10 +6,20 @@ data class Account(val iban: String, var balance: Double = 0.0) {
         // initialization
     }
 
-    fun deposit(amount: Double) {}
+    fun deposit(amount: Double) {
+        balance += amount
+        println("Dépot d'un montant de $amount€ par $iban")
+    }
 
-    fun withdraw(amount: Double) {}
+    fun withdraw(amount: Double) {
+        balance -= amount
+        println("Débit d'un montant de $amount€ par $iban")
+    }
 
-    fun transfer(receiver: Account, amount: Double) {}
+    fun transfer(receiver: Account, amount: Double) {
+        balance -= amount
+        receiver.deposit(amount)
+        println("Transfert d'un montant de $amount€ par $iban à ${receiver.iban}")
+    }
 
 }
--- src/main/kotlin/com/teaching/project/banking/domain/Bank.kt
+++ src/main/kotlin/com/teaching/project/banking/domain/Bank.kt
@@ -2,10 +2,22 @@ package com.teaching.project.banking.domain;
 
 data class Bank(val accounts: MutableMap<String, Account> = mutableMapOf()) {
 
-    fun register(account: Account) {}
+    fun register(account: Account) {
+        accounts[account.iban] = account
+        println("---------------")
+        println("Ajout d'un compte : $account ")
+        println("---------------")
+    }
 
     fun transfer(sender: Account, receiver: Account, amount: Double) {}
 
-    fun dashboard() {}
+    fun dashboard() {
+        println("---------------")
+        println("Liste de tous les comptes :")
+        for (account in accounts)  {
+            println(account)
+        }
+        println("---------------")
+    }
 
 }
